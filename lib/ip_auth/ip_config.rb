55973ea06d8eb9b3a13f431f9510d08bbb281975
Anand Mohanaveeramani <manand777@gmail.com>
2013-08-11 21:23:08 +0530

    privteize

--- lib/ip_auth/ip_config.rb
+++ lib/ip_auth/ip_config.rb
@@ -24,28 +24,30 @@ module IpAuth
       end
     end
 
-    def get_configuration(options = {})
-      update_decoded_setting unless @decoded_setting_valid
-      @decoded_setting
-    end
+    private
 
-    def set_configuration(options = {})
-      raise IpAuth::Exception::General.new("Ip configuration is not a Array") unless options[:config].is_a?(Array)
-      @decoded_setting_valid = false
-      self.update(setting: get_encoded_string(options[:config]))
-      get_configuration(options)
-    end
-
-    def get_encoded_string(conf)
-      conf.collect(&:to_json).join(IP_CONFIG_UNIT_SEPARATOR)
-    end
-
-    def update_decoded_setting
-      @decoded_setting = []
-      self.setting.split(IP_CONFIG_UNIT_SEPARATOR).each do |unit_str|
-        @decoded_setting << IpAuth::IpConfigUnit.new(JSON.load(unit_str))
+      def get_configuration(options = {})
+        update_decoded_setting unless @decoded_setting_valid
+        @decoded_setting
       end
-      @decoded_setting_valid = true
-    end
+
+      def set_configuration(options = {})
+        raise IpAuth::Exception::General.new("Ip configuration is not a Array") unless options[:config].is_a?(Array)
+        @decoded_setting_valid = false
+        self.update(setting: get_encoded_string(options[:config]))
+        get_configuration(options)
+      end
+
+      def get_encoded_string(conf)
+        conf.collect(&:to_json).join(IP_CONFIG_UNIT_SEPARATOR)
+      end
+
+      def update_decoded_setting
+        @decoded_setting = []
+        self.setting.split(IP_CONFIG_UNIT_SEPARATOR).each do |unit_str|
+          @decoded_setting << IpAuth::IpConfigUnit.new(JSON.load(unit_str))
+        end
+        @decoded_setting_valid = true
+      end
   end
 end
